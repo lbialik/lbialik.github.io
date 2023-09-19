3c61d5c3097a4a76eec603b3d99d8762eb1259a4
Liza Bialik <liza.bialik@gmail.com>
2023-09-19 02:44:53 -0400

    extract gallery + featured image logic into gallery.js

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,11 +1,26 @@
 // gallery.js
 
+// Function to generate entire gallery, featured image included
+function generateFeatured(imageUrl, featuredId) {
+  // Create a new <img> element
+  const imgElement = document.createElement("img");
+
+  // Set the attributes for the <img> element
+  imgElement.id = featuredId + "Img";
+  imgElement.src = imageUrl;
+  imgElement.alt = "Featured Image";
+  imgElement.className = "featured-image";
+
+  var parentElement = document.getElementById(featuredId);
+  parentElement.appendChild(imgElement);
+}
+
 // Function to generate gallery items from a list of image URLs
-function generateGallery(imageUrls, galleryID) {
-  const galleryElement = document.getElementById(galleryID);
+function generateGallery(imageUrls, galleryId) {
+  const galleryElement = document.getElementById(galleryId);
 
   if (!galleryElement) {
-    console.error(`Gallery element with ID '${galleryId}' not found.`);
+    console.error(`Gallery element with Id '${galleryId}' not found.`);
     return;
   }
 
@@ -16,7 +31,7 @@ function generateGallery(imageUrls, galleryID) {
     const imgElement = document.createElement("img");
     imgElement.src = imageUrl;
     imgElement.alt = "Image " + (index + 1);
-    imgElement.onclick = () => setFeatured(imageUrl);
+    imgElement.onclick = () => setFeatured(imageUrl, galleryId);
 
     galleryItem.appendChild(imgElement);
     galleryElement.appendChild(galleryItem);
@@ -24,6 +39,6 @@ function generateGallery(imageUrls, galleryID) {
 }
 
 // Function to set the featured image
-function setFeatured(imageUrl, galleryID) {
-  document.getElementById(galleryID + "featured").src = imageUrl;
+function setFeatured(imageUrl, galleryId) {
+  document.getElementById(galleryId + "FeaturedImg").src = imageUrl;
 }
